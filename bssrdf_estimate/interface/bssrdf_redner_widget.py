5c3a54a79a763b3b761f053f7360ad2d670ae5cb
tatsy <tatsy.mail@gmail.com>
2015-07-20 00:58:10 +0900

    Add BSSRDF render widget file.

---
+++ bssrdf_estimate/interface/bssrdf_redner_widget.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from .image_widget import ImageWidget
+
+class BSSRDFRenderWidget(ImageWidget):
+    def __init__(self, parent=None):
+        super(BSSRDFRenderWidget, self).__init__(parent)
